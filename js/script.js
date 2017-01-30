5fe62c2beba4cffd284341b0ae8fcdd55d0a0b4d
Chris Jespersen <cwjespersen@gmail.com>
2017-01-29 23:29:53 -0500

    ;

--- js/script.js
+++ js/script.js
@@ -6,25 +6,26 @@ var changeState = function (state) {
 	clearInterval(timer);
 	countdownNumber = 10;
 	document.getElementById('countdown').innerHTML = countdownNumber;
-
+}
 	//countdown
 	if(state ==2) {
 		timer = setInterval(function () {
 			countdownNumber = countdownNumber -1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
+			
 				if (countdownNumber > 4 && countdownNumber <= 7) {
 					// be nervous
 					document.getElementById('nervous').className = 'nervous show';
 				} else	{
 					document.getElementById('nervous').className = 'nervous';
-				};
+				}
 
 				if (countdownNumber > 1 && countdownNumber <= 4) {
 					// can't wait
 					document.getElementById('cant-wait').className = 'cant-wait show';
 				} else {
 					document.getElementById('cant-wait').className = 'cant-wait';
-				};
+				}
 
 				if (countdownNumber <= 0) {
 					changeState(3);
