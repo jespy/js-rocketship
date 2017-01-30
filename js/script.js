d0b51d3d6989ea9512342ae48fd20bfb33383899
Chris Jespersen <cwjespersen@gmail.com>
2017-01-29 23:25:46 -0500

    semi colons

--- js/script.js
+++ js/script.js
@@ -12,20 +12,19 @@ var changeState = function (state) {
 		timer = setInterval(function () {
 			countdownNumber = countdownNumber -1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
-				document
 				if (countdownNumber > 4 && countdownNumber <= 7) {
 					// be nervous
 					document.getElementById('nervous').className = 'nervous show';
 				} else	{
 					document.getElementById('nervous').className = 'nervous';
-				}
+				};
 
 				if (countdownNumber > 1 && countdownNumber <= 4) {
 					// can't wait
 					document.getElementById('cant-wait').className = 'cant-wait show';
 				} else {
 					document.getElementById('cant-wait').className = 'cant-wait';
-				}
+				};
 
 				if (countdownNumber <= 0) {
 					changeState(3);
